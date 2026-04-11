b60a14ec10ecac15db01eaaab1a52792cd44b66a
seria <seria.ati@gmail.com>
2026-04-11 10:54:34 +0900

    fix: Update default database name to 'roler'

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     env: Literal["dev", "prod"]
 
     postgres_password: str
-    postgres_db: str = "number-gv-bot"
+    postgres_db: str = "roler"
     postgres_host: str = "localhost"
     postgres_port: int = 5432
     postgres_user: str = "postgres"
